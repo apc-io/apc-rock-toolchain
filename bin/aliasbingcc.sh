1184bd0195797d6532ec683ed6c3ac98caa12273
Sean Moss-Pultz <sean@openmoko.com>
2013-06-20 22:49:25 +0800

    Initial commit of toolchain.
    Signed-off-by: Sean Moss-Pultz <sean@openmoko.com>

---
+++ bin/aliasbingcc.sh
@@ -0,0 +1,6 @@
+alias arm-none-linux-gnueabi-as='arm-none-linux-gnueabi-as -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=softfp'
+alias arm-none-linux-gnueabi-c++='arm-none-linux-gnueabi-c++ -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=softfp'
+alias arm-none-linux-gnueabi-cpp='arm-none-linux-gnueabi-cpp -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=softfp'
+alias arm-none-linux-gnueabi-g++='arm-none-linux-gnueabi-g++ -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=softfp'
+alias arm-none-linux-gnueabi-gcc='arm-none-linux-gnueabi-gcc -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=softfp'
+alias arm-none-linux-gnueabi-gcc-4.5.2='arm-none-linux-gnueabi-gcc-4.5.2 -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=softfp'
---
+++ mybin/aliasmybingcc.sh
@@ -0,0 +1,6 @@
+alias arm_1103_le-as='arm_1103_le-as -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=softfp'
+alias arm_1103_le-c++='arm_1103_le-c++ -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=softfp'
+alias arm_1103_le-cpp='arm_1103_le-cpp -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=softfp'
+alias arm_1103_le-g++='arm_1103_le-g++ -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=softfp'
+alias arm_1103_le-gcc='arm_1103_le-gcc -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=softfp'
+alias arm_1103_le-gcc-4.5.2='arm_1103_le-gcc-4.5.2 -mcpu=cortex-a9 -mfpu=neon -mfloat-abi=softfp'
